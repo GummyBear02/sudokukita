0bb4a2c2d521c3b4018ea5cb9f31bc23f4cf52a4
NiskoDT <65821447+NiskoDT@users.noreply.github.com>
2025-11-24 12:28:54 +0700

    Revert "Beutified titles"
    This reverts commit 7f9b9c088b34318778f4433246143d27da68bea5.

--- sudoku_4x4.c
+++ sudoku_4x4.c
@@ -380,26 +380,7 @@ int main(int argc, char **argv) {
   for (int i=0;i<N;i++) for (int j=0;j<N;j++) given[i][j] = (board[i][j] != 0);
 
   char line[64];
-
-  // Judul program improvisasi
-  // ? Judul awal
-  printf("=========================\n");
-  printf("|\tSUDOKU 4x4\t|\n");
-  printf("=========================\n");
-  // ? Petunjuk
-  printf("Petunjuk bermain:\n");
-  printf("1. Masukkan dengan format seperti berikut\n"); 
-  printf("\n");
-  printf("   ==> baris, kolom, nilai <==\n\n");
-  printf("   Misalkan: 2 3 4\n");
-  printf("   Baris 2, Kolom 3, dan Nilai 4\n");
-  printf("\n");
-  printf("2. Ketik 'reveal' untuk melihat solusi\n");
-  printf("3. Ketik 'q' untuk keluar\n");
-  printf("\n");
-  // ? judul akhir
-  printf("=== PERMAINAN DIMULAI ===\n");
-  printf("\n");
+  printf("4x4 Sudoku. Perintah: 'baris kolom nilai' (mis: 2 3 4), 'reveal' untuk solusi, 'q' keluar.\n\n");
 
   while (1) {
     print_board(board);
@@ -419,7 +400,7 @@ int main(int argc, char **argv) {
     if (given[r][c]) { printf("Posisi (%d,%d) adalah given. Tidak bisa diubah.\n", br, bc); continue; }
     if (solution[r][c] != v) { printf("Salah. Coba lagi.\n"); continue; }
     board[r][c]=v;
-    printf("\nTerisi (%d,%d) = %d\n\n", br, bc, v);
+    printf("Terisi (%d,%d) = %d\n\n", br, bc, v);
   }
 
   return 0;
