4530698c41996df544ab6acd6d6eee659237b2f9
NiskoDT <65821447+NiskoDT@users.noreply.github.com>
2025-11-24 13:59:49 +0700

    quick compile guide fix

--- sudoku_4x4.c
+++ sudoku_4x4.c
@@ -1,7 +1,11 @@
 /*
  * 4x4 Sudoku generator + interactive player
- * Compile: gcc -O2 -Wall -Wextra '.\sudoku_4x4.c' -o .\sudoku.exe
- * Run:     ./sudoku.exe 6   (arg = jumlah blanks, default 6)
+ * Compile: 
+ * PowerShell: 
+ * gcc -O2 -Wall -Wextra '.\sudoku_4x4.c' -o .\sudoku.exe
+ * 
+ * Run:    
+ * ./sudoku.exe 6   (arg = jumlah blanks, default 6)
  */
 
 #include <stdio.h>
